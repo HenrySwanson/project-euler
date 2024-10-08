89a5a2162f2ad708164afbbb88b82925beb1b0e8
Henry Swanson <henryswanson94@gmail.com>
2024-10-08 11:08:34 -0400

    Solve 89 and 90

--- problems/0089.py
+++ problems/0089.py
@@ -19,6 +19,49 @@ Find the number of characters saved by writing each of these in their minimal fo
 Note: You can assume that all the Roman numerals in the file contain no more than four consecutive identical units.
 """
 
+from typing import Tuple
+
+SIMPLEST_STRS = "|I|II|III|IV|V|VI|VII|VIII|IX".split("|")
+
 
 def solve_problem() -> int:
-    ...
+    with open("resources/p089_roman.txt") as f:
+        numerals = f.readlines()
+
+    return sum(get_character_excess(r) for r in numerals)
+
+
+def get_character_excess(roman: str) -> int:
+    # chomp through the Ms, since they'll always be Ms
+    i = 0
+    while i < len(roman) and roman[i] == "M":
+        i += 1
+
+    # helper fn to avoid repeating things a bunch
+    def _parse_roman_numeral(ones: str, fives: str, tens: str) -> int:
+        nonlocal i
+        start = i
+        total = 0
+        while i < len(roman):
+            ch = roman[i]
+            if ch == ones:
+                total += 1
+            elif ch == fives:
+                total += 5
+            elif ch == tens:
+                assert total == 1
+                total = 10 - total
+            else:
+                break
+
+            i += 1
+
+        # take the length of our substring and subtract off the ideal length
+        return (i - start) - len(SIMPLEST_STRS[total])
+
+    # investigate the hundreds place: CDM
+    excess_hundreds = _parse_roman_numeral("C", "D", "M")
+    excess_tens = _parse_roman_numeral("X", "L", "C")
+    excess_ones = _parse_roman_numeral("I", "V", "X")
+
+    return excess_hundreds + excess_tens + excess_ones
--- problems/0090.py
+++ problems/0090.py
@@ -3,9 +3,7 @@ Each of the six faces on a cube has a different digit (0 to 9) written on it; th
 
 For example, the square number 64 could be formed:
 
-<img src=project/images/p090.png,class=dark_img,alt=></img>
-
-
+<image>
 
 In fact, by carefully choosing the digits on both cubes it is possible to display all of the square numbers below one-hundred: 01, 04, 09, 16, 25, 36, 49, 64, and 81.
 
@@ -23,6 +21,102 @@ But because we are allowing 6 and 9 to be reversed, the two distinct sets in the
 How many distinct arrangements of the two cubes allow for all of the square numbers to be displayed?
 """
 
+from typing import List
+
+# Put 7 last because it's the only digit we don't use,
+# and put 6 and 9 near the end because they're strange.
+DIGITS = [0, 1, 2, 3, 4, 5, 8, 6, 9, 7]
+
+SQUARES = [x * x for x in range(1, 10)]
+SQUARE_DIGITS = [(x // 10, x % 10) for x in SQUARES]
+SQUARE_MAP = {
+    x: [
+        y
+        for y in range(10)
+        if x >= y and ((x,y) in SQUARES or (y, x) in SQUARES)
+    ]
+    for x in range(10)
+}
+
 
 def solve_problem() -> int:
-    ...
+    return foo([], [], 0)
+
+def foo(cube_a: List[int], cube_b: List[int], idx: int) -> int:
+    # Are we at the end of the digit list? If so, our cubes are nearly
+    # complete.
+    if idx == len(DIGITS):
+        if is_correct(cube_a, cube_b):
+            # Deduplicate
+            if tuple(cube_a) <= tuple(cube_b):
+                return 1
+        return 0
+
+    # We try placing the number on one cube, the other cube, both, or
+    # neither.
+    total = 0
+    digit = DIGITS[idx]
+
+    # Neither cube
+    if is_viable(cube_a, cube_b, hint=digit):
+        total += foo(cube_a, cube_b, idx + 1)
+
+    # Just on cube A
+    cube_a.append(digit)
+    if is_viable(cube_a, cube_b, hint=digit):
+        total += foo(cube_a, cube_b, idx + 1)
+
+    # On both cubes
+    cube_b.append(digit)
+    if is_viable(cube_a, cube_b, hint=digit):
+        total += foo(cube_a, cube_b, idx + 1)
+
+    # Only on cube B
+    cube_a.pop()
+    if is_viable(cube_a, cube_b, hint=digit):
+        total += foo(cube_a, cube_b, idx + 1)
+
+    # Restore cubes to original state
+    cube_b.pop()
+    return total
+
+
+def is_viable(cube_a: List[int], cube_b: List[int], hint: int) -> bool:
+    # First, are both cubes <= 6 sides?
+    if len(cube_a) > 6 or len(cube_b) > 6:
+        return False
+
+    # 6s and 9s are weird, just optimistically say yes.
+    # (Why? Because we get a 'second chance' to spell squares with 6s and 9s. Just
+    # because we can't spell it now doesn't mean we can't spell it later.)
+    if hint == 6 or hint == 9:
+        return True
+    
+    # Next, check that it's possible to spell the desired squares with these
+    # cubes. Previous calls to is_viable have checked other squares, so we
+    # only check those where the biggest digit is `hint`.
+    for d in SQUARE_MAP[hint]:
+        if not can_spell(cube_a, cube_b, d, hint):
+            return False
+
+    return True
+
+def is_correct(cube_a: List[int], cube_b: List[int]) -> bool:
+    if len(cube_a) != 6 or len(cube_b) != 6:
+        return False
+
+    for (x, y) in SQUARE_DIGITS:
+        if not can_spell(cube_a, cube_b, x, y):
+            return False
+    return True
+
+def can_spell(cube_a: List[int], cube_b: List[int], d1: int, d2: int) -> bool:
+    # Check that it's spellable as AB or BA or both
+    ab = has_digit_or_69(cube_a, d1) and has_digit_or_69(cube_b, d2)
+    ba = has_digit_or_69(cube_b, d1) and has_digit_or_69(cube_a, d2)
+    return ab or ba
+
+def has_digit_or_69(cube: List[int], digit: int) -> bool:
+    if digit == 6 or digit == 9:
+        return 6 in cube or 9 in cube
+    return digit in cube
