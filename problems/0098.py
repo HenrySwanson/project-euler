d8b8f491f4c4465ab1e470df1c76d9e8b8321ad3
Henry Swanson <henryswanson94@gmail.com>
2024-10-10 14:21:49 -0400

    Solve 98 and 100 - first 100 complete! :)

--- problems/0098.py
+++ problems/0098.py
@@ -1,13 +1,88 @@
 """
-By replacing each of the letters in the word CARE with 1, 2, 9, and 6 respectively, we form a square number: 1296 = 36^2. What is remarkable is that, by using the same digital substitutions, the anagram, RACE, also forms a square number: 9216 = 96^2. We shall call CARE (and RACE) a square anagram word pair and specify further that leading zeroes are not permitted, neither may a different letter have the same digital value as another letter.
+By replacing each of the letters in the word CARE with 1, 2, 9, and 6
+respectively, we form a square number: 1296 = 36^2. What is remarkable is that,
+by using the same digital substitutions, the anagram, RACE, also forms a square
+number: 9216 = 96^2. We shall call CARE (and RACE) a square anagram word pair
+and specify further that leading zeroes are not permitted, neither may a
+different letter have the same digital value as another letter.
 
-Using words.txt (right click and 'Save Link/Target As...'), a 16K text file containing nearly two-thousand common English words, find all the square anagram word pairs (a palindromic word is NOT considered to be an anagram of itself).
+Using words.txt (right click and 'Save Link/Target As...'), a 16K text file
+containing nearly two-thousand common English words, find all the square anagram
+word pairs (a palindromic word is NOT considered to be an anagram of itself).
 
 What is the largest square number formed by any member of such a pair?
 
 NOTE: All anagrams formed must be contained in the given text file.
 """
 
+from collections import defaultdict
+import itertools
+import json
+from math import isqrt
+from typing import Dict, Iterable, List, Tuple
+
+from lib.misc import from_digits, to_digits
+
 
 def solve_problem() -> int:
-    ...
+    # Load words
+    with open("resources/0098_words.txt") as f:
+        # hack to read the quoted words
+        words = json.loads("[" + f.read() + "]")
+
+    # Detect anagrams
+    word_anagrams = make_anagram_map(words)
+    for k, v in word_anagrams.items():
+        # print(k, v)
+        pass
+
+    max_len = max(len(w) for w in word_anagrams)
+    assert max_len == 9
+
+    # Generate anagrammable squares
+    square_anagrams = make_anagram_map(to_digits(x * x) for x in range(isqrt(10**9)))
+
+    # Now we mash them together
+    best = 0
+    for letters, words in word_anagrams.items():
+        for digits, squares in square_anagrams.items():
+            if len(digits) != len(letters):
+                continue
+
+            # Now we can try to match up the squares with the words
+            for w1, w2 in itertools.permutations(words, 2):
+                for s1, s2 in itertools.permutations(squares, 2):
+                    if test(w1, w2, s1, s2):
+                        best = max(best, from_digits(s1), from_digits(s2))
+
+    return best
+
+
+def make_anagram_map(seqs: Iterable[Iterable[str]]) -> Dict[Tuple[str, ...], List[str]]:
+    map = defaultdict(list)
+    for seq in seqs:
+        sorted_seq = tuple(sorted(seq))
+        map[sorted_seq].append(seq)
+    return {k: v for k, v in map.items() if len(v) > 1}
+
+
+def test(w1: List[str], w2: List[str], s1: List[str], s2: List[str]) -> bool:
+    mapping = {}
+    assert len(w1) == len(s1)
+    for ch, d in zip(w1, s1):
+        if ch in mapping:
+            if mapping[ch] != d:
+                return False
+        else:
+            mapping[ch] = d
+
+    assert len(w2) == len(s2)
+    for ch, d in zip(w2, s2):
+        if ch in mapping:
+            if mapping[ch] != d:
+                return False
+        else:
+            mapping[ch] = d
+
+    # No duplicate digits!
+    return len(set(mapping.values())) == len(mapping)
--- problems/0100.py
+++ problems/0100.py
@@ -1,11 +1,60 @@
 """
-If a box contains twenty-one coloured discs, composed of fifteen blue discs and six red discs, and two discs were taken at random, it can be seen that the probability of taking two blue discs, P(BB) = (15/21)×(14/20) = 1/2.
+If a box contains twenty-one coloured discs, composed of fifteen blue discs and
+six red discs, and two discs were taken at random, it can be seen that the
+probability of taking two blue discs, P(BB) = (15/21)×(14/20) = 1/2.
 
-The next such arrangement, for which there is exactly 50% chance of taking two blue discs at random, is a box containing eighty-five blue discs and thirty-five red discs.
+The next such arrangement, for which there is exactly 50% chance of taking two
+blue discs at random, is a box containing eighty-five blue discs and thirty-five
+red discs.
 
-By finding the first arrangement to contain over 10^12 = 1,000,000,000,000 discs in total, determine the number of blue discs that the box would contain.
+By finding the first arrangement to contain over 10^12 = 1,000,000,000,000 discs
+in total, determine the number of blue discs that the box would contain.
 """
+
+LIMIT = 10**12
 
 
 def solve_problem() -> int:
+    # This is another Pell equation problem.
+    #
+    # We need to find integer b and t (blue and total) such that b/t*(b-1)/(t-1)
+    # is exactly 1/2.
+    #
+    # Rearranging that, we get 2b(b-1) = t(t-1). Taking the solutions from the
+    # example, we get (b,t) = (15,21); does 2*15*14 = 21*20? Yep.
+    #
+    # Completing the square for x(x-1) is (x-1/2)^2 - 1/4, so let's multiply
+    # through by 4: 4x(x-1) = (2x-1)^2 - 1.
+    # Applying that to our equation:
+    #     2 [(2b-1)^2 - 1] = (2t-1)^2 - 1
+    #     -1 = (2t-1)^2 - 2(2b-1)^2
+    #
+    # So, with x = 2t-1, y = 2b-1, we have:
+    #     x^2 - 2y^2 = -1
+    #
+    # Okay so it's negative Pell's equation. Close enough.
+    # The original solution has (x,y) = (41,29), and indeed, 41^2 - 2*29^2 is -1.
+    #
+    # The fundamental solution for this one is pretty easy to guess: (1,1).
+    # It doesn't correspond to a realistic urn (b=1, t=1), but that's okay, we
+    # don't need it to.
+    #
+    # Remember to take odd powers only (or else you get +1).
+
+    x, y = 1, 1
+    while True:
+        # TODO: factor out pell multiplication into some helper function
+        # (x+y√2)(1+√2) = (x + 2y) + (x + y)√2
+        x, y = x + 2 * y, x + y
+        x, y = x + 2 * y, x + y
+
+        # Sanity check
+        assert x * x - 2 * y * y == -1
+
+        # Get total number of discs
+        t = (x + 1) // 2
+
+        if t > LIMIT:
+            b = (y + 1) // 2
+            return b
     ...
